3cc66db3afe36a596fc52045fba3fbdc6a2bcee5
Evgeniy <evolkovskiy@12devs.com>
2024-08-03 10:48:00 +0300

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+function HomePage() {
+  return <>
+    This is HomePage
+  </>;
+}
+
+function AboutPage() {
+  return <>
+    This is AboutPage
+  </>;
+}
 
 function App() {
   return (
@@ -7,6 +20,15 @@ function App() {
         <p>
           Hello Gd-FD3-21-24!
         </p>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
